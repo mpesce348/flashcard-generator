44f74a4bc49a46799b6649470c454c08a1adc64f
Michael Pesce <Michael Pesce>
2017-06-13 19:27:40 -0400

    improved pseudo code

--- basicCard.js
+++ basicCard.js
@@ -1,3 +1,4 @@
+//set variables to require packages and other files in the directory
 var inquirer = require('inquirer');
 var fs = require('fs');
 
@@ -12,14 +13,16 @@ var BasicCard = function(front, back) {
 	// The constructed object should have a back property that contains
 	//  the text on the back of the card.
 	this.back = back;
-
+	//once question and anwsers values are passed they are console logged
 	console.log('Front: ' + this.front + " Back: " + this.back + '.');
-
+	//uses file system package to append question and anwser values to the basic.json file
+	//also sets up function to display errors should one occur
 	fs.appendFile("basic.json", "{front: " + this.front + ", back: " + this.back + "},", function(err){
 			if (err) console.log(err)
 		})
 }
-
+//sets up function to to utalize inquirer package to promt user for 
+//question and anwser info
 var BasicCardInput = function(){
 	inquirer.prompt([
 
@@ -33,15 +36,17 @@ var BasicCardInput = function(){
 			message: 'Enter Anwser',
 			name: 'backText'
 		}
+		//once values have been passed, runs enterInputs function
 		]).then(enterInputs);
 }
-
+//sets up function to capture inputs and calls the basic card function
+//using values passed by the user
 var enterInputs = function(inputs){
 	new BasicCard(inputs.frontText, inputs.backText);
 	
 }
-
+//runs the function to promt user to enter question/anwser data
 BasicCardInput();
 
-
+//exports the data captured in the BasicCardInput function
 module.exports = BasicCardInput;
--- flashCardGame.js
+++ flashCardGame.js
@@ -1,2 +1,4 @@
 var inquirer = require('inquirer');
 var cardData = require('./basic.json');
+var basicCards = require('./basicCard.js');
+var clozeCards = require('clozeCard.js');
