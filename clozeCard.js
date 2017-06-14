b9da2d0daabf7c62e0b3a341151a6217f7285489
Michael Pesce <Michael Pesce>
2017-06-13 22:30:43 -0400

    for review

--- clozeCard.js
+++ clozeCard.js
@@ -1,3 +1,4 @@
+//set variables to require packages and other files in the directory
 var fs = require('fs');
 var inquirer = require('inquirer');
 
@@ -5,7 +6,7 @@ var inquirer = require('inquirer');
 //  for creating cloze-deletion flashcards,
 //   e.g.: module.exports = ClozeCard;
 // The constructor should accept two arguments: text and cloze.
-var ClozeCard =function(partial, fullText, err) {
+var ClozeCard =function(partial, fullText, cloze, err) {
 // The constructed object should have a cloze property that contains
 //  only the cloze-deleted portion of the text.
 	this.partial = partial;
@@ -15,15 +16,23 @@ var ClozeCard =function(partial, fullText, err) {
 	this.fullText = fullText,
 // The constructed object should have a fullText property that
 //  contains only the full text.
-	this.err = err;
+	this.cloze = cloze,
+	
 // The constructor should throw or log an error when the cloze
 //  deletion does not appear in the input text.
+fs.appendFile("cloze.json", "{full text: " + this.fullText + ", cloze argument: " + this.cloze + ", partial text: " + this.partial + "},", function(err){
+			if (err) console.log(err)
+		})
 
 }
 // Use prototypes to attach these methods, wherever possible.
 var ClozeCardInput = function(){
 	inquirer.prompt([
-
+		{
+			type: 'input',
+			message: 'Enter The Full Text',
+			name: 'fullText'
+		}
 		{
 			type: 'input',
 			message: 'Enter Cloze Argument',
@@ -32,13 +41,13 @@ var ClozeCardInput = function(){
 		{
 			type: 'input',
 			message: 'Enter The Rest Of The Sentence',
-			name: 'fullText'
+			name: 'partial'
 		}
 		]).then(enterInputs);
 }
 
 var enterInputs = function(inputs){
-	new ClozeCard(inputs.clozeText, inputs.fullText);
+	new ClozeCard(inputs.fullText, inputs.clozeText, inputs.partial);
 	
 }
 
--- flashCardGame.js
+++ flashCardGame.js
@@ -1,4 +1,47 @@
+//set variables to require packages and other files in the directory
 var inquirer = require('inquirer');
-var cardData = require('./basic.json');
+var basicCardData = require('./basic.json');
+var clozeCardData = require('./cloze.json');
 var basicCards = require('./basicCard.js');
-var clozeCards = require('clozeCard.js');
+var clozeCards = require('./clozeCard.js');
+//sets up function to promt user to chose to make new cards or to quiz themselves
+var userProcess = function(){
+
+	inquirer.prompt ([
+		{
+			type: 'input',
+			message: 'Type "build" to make new flashcards or type "play" to quiz yourself',
+			name: 'buildOrPlay'
+		}
+		]).then(handleInput);
+
+};
+//sets up function with switch case to process the user's choice to build or play
+var handleInput = function(processChoice) {
+	switch (processChoice.buildOrPlay) {
+		case 'build':
+			cardType();
+			break;
+		case 'play':
+			playQuiz();
+			break;
+		default:
+			console.log('Invalid Entry - Try Again');
+	}
+};
+
+var cardType = function() {
+
+};
+
+var playQuiz = function() {
+
+};
+
+
+
+
+
+
+//runs user promt
+userProcess();
